4626cbc8e3bc1c7bcb21379e053fec685b024ce2
nazanin.b <baghernejad.it89@gmail.com>
2024-10-03 23:43:36 +0330

    change css

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -6,8 +6,8 @@ function AddNewNote() {
     <div className="add-new-note">
       <h2>Add new note</h2>
       <form className="note form" onSubmit={handleSubmit}>
-        <input type="text" className="text-field"/>
-        <input type="text" className="text-field"/>
+        <input type="text" className="text-field" placeholder="Note title"/>
+        <input type="text" className="text-field" placeholder="Note description"/>
         <button type="submit" className="btn btn--primary">Add New Note</button>
       </form>
     </div>
