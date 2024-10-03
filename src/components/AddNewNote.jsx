8645625a234d110e236f6effbd64c2e0a32dc46f
nazanin.b <baghernejad.it89@gmail.com>
2024-10-03 23:59:22 +0330

    hook

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 function AddNewNote() {
-    const handleSubmit= (event)=>{
-        event.preventDefault();
+  const[title,setTitle]= useState("");
+  const[description,setDescription]= useState("");
+  const handleSubmit= (event)=>{
+    event.preventDefault();
     };
+  // const handleChange= (event)=>{
+  //   setTitle(event.target.value);
+  // };
   return (
     <div className="add-new-note">
       <h2>Add new note</h2>
       <form className="note form" onSubmit={handleSubmit}>
-        <input type="text" className="text-field" placeholder="Note title"/>
-        <input type="text" className="text-field" placeholder="Note description"/>
+        <input 
+          value={title} 
+          onChange={(event)=> settitle(event.target.value)} 
+          type="text" 
+          className="text-field" 
+          placeholder="Note title"/>
+        <input 
+        value={description}
+        onChange={event =>setDescription(event.target.value)}
+        type="text" 
+        className="text-field" 
+        placeholder="Note description"/>
         <button type="submit" className="btn btn--primary">Add New Note</button>
       </form>
     </div>
